581be75c2b78a3d129b3aa00e1b76530d51e6f6c
LeonathaTurtle <mancillh@gmail.com>
2024-10-13 15:03:56 -0500

    added favicon

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function JSXVariables() {
+    return (
+        <div className="container">
+          <div className="">
+            <h1>Hi! My name is Hannah Mancill</h1>
+            <h2>Full-Stack Web Developer</h2>
+          </div>
+        </div>
+    );
+  }
+  
+  export default JSXVariables;
