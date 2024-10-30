3b046f505a136a4428e0101c1c2f02fd50a9e701
LeonathaTurtle <mancillh@gmail.com>
2024-10-30 12:56:10 -0500

    edited About Me section to include all modules of bootcamp

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -18,13 +18,16 @@ export default function AboutPage() {
                     <Col lg={9}>
                     <p className="about">Hi! Welcome to a website I proudly designed.</p>
 
-                        <p className="about">I earned a certificate in full-stack web development through Rice University in October 2024!
-                            I have experience with Node, Express, MongoDB, SQL, jQuery, JavaScript, HTML, Jest, Handlebars, React, 
-                            CSS and the Semantic UI, Bootstrap and Bulma CSS frameworks.</p>
+                        <p className="about">I earned a certificate in full-stack web development through Rice 
+                            University in October 2024! I have experience with Git, Node, Express, MongoDB, 
+                            Mongoose, SQL, Sequelize, jQuery, JavaScript, HTML, Jest, Handlebars, React, 
+                            MERN-Stack Apps, Web APIs, Third-Party APIs, Server-Side APIs, Object Oriented 
+                            Programming, the Model-View-Controller Architectural Pattern, Progressive Web 
+                            Applications, CSS and the Semantic UI, Bootstrap and Bulma CSS frameworks.</p>
 
-                        <p className="about">I work part time as IT for a small church and moderate high school Quiz Bowl tournaments. In my free
-                            time I enjoy baking, reading, trivia, breaking out of escape rooms, creating puzzles, and spending time
-                            with my family.</p>
+                        <p className="about">I work part time as IT for a small church and moderate high school 
+                            Quiz Bowl tournaments. In my free time I enjoy baking, reading, trivia, breaking out 
+                            of escape rooms, creating puzzles, and spending time with my family.</p>
                     </Col>
                 </Row>
             </Container>
