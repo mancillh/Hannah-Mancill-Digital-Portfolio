9203f7fe7ec902868fb7061fdb72825a7dc5e0b6
LeonathaTurtle <mancillh@gmail.com>
2024-11-05 20:11:46 -0600

    added empty html tags to help footer render

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import '../styles/Footer.css';
 // Renders Footer Component
 export default function Footer() {
     return (
+        <>
         <footer>
             <h6>&copy; 2024 Hannah Mancill</h6>
             <div className="footer-links">
@@ -11,5 +12,6 @@ export default function Footer() {
                 <a href='https://www.linkedin.com/in/hannah-mancill-aa37583b/'><img src='/assets/LinkedIn_logo_initials.png' alt='LinkedIn logo'></img></a>
             </div>
         </footer>
+        </>
     );
 }
